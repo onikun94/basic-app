7dd8859522ef9fb8d4981e17a6a9debbf1a36e74
onikun94 <onikun94@gmail.com>
2021-01-15 15:08:37 +0900

    set react basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import BasicLifeCycle from './components/BasicLifeCycle';
+import BasicProps from './components/BasicProps'
+import BasicState from './components/BasicState'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicProps name ={"propsの基本"}/>
+      <BasicState />
+      <BasicLifeCycle />
     </div>
   );
 }
