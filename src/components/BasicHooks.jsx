2f2bcf4299510d6cac7ded7c20faa2b794de1739
onikun94 <onikun94@gmail.com>
2021-01-15 21:51:22 +0900

    about Hooks

--- src/components/BasicHooks.jsx
+++ src/components/BasicHooks.jsx
@@ -3,13 +3,22 @@ import React, {useState} from 'react'
 const BasicHooks = () => {
 
     const [isTest, toggleTest] = useState(false);
-    const [unkoFun, toggleUnko] = useState("加藤純一最強");
+    const [unkoFun, changeUnko] = useState("加藤純一最強");
     console.log(isTest);
+
+    const ChangeKato = () =>{
+        if(isTest){
+            changeUnko("やあ");
+        }else{
+            changeUnko("もえ〜")
+        }
+    }
+
     return (
         <div>
             <h2>Hooksについて</h2>
             <h3>{unkoFun}</h3>
-            <input type="checkbox" checked = {isTest} onClick = {() => toggleTest(!isTest) }/>
+            <input type="checkbox" checked = {isTest} onClick = {() => toggleTest(!isTest, ChangeKato()) }/>
             
         </div>
     )
